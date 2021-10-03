388848a62b043ea3304508d567b3d36dbef41a45
ImSoHappy <D1034181036@gm.lhu.edu.tw>
2021-10-03 15:25:15 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function getDownloadLink(){
+	$('#videoSubmitButton').attr("disabled", true);
+	$("#loadingSection").removeClass('d-none');
+	$("#resultSection").addClass('d-none');
+	$("#videoUrl").removeClass('is-invalid');
+	
+	const videoUrl = $('#videoUrl').val();
+	
+	$.post("api.php", {videoUrl: videoUrl})
+	.done(function(data){
+		try{
+			const videoObj = JSON.parse(data);
+			const targetVideo = videoObj['formats'].slice(-1)[0];
+			const videoFormatSting = targetVideo['format'].substr(targetVideo['format'].indexOf('-')+2);
+			$("#videoThumbnail").attr('src', videoObj['thumbnail']);
+			$("#videoTitle").text(videoObj['title']);
+			$("#videoChannel").text(videoObj['channel']);
+			$("#videoFormat").text(videoFormatSting);
+			$("#videoDownloadButton").attr('href', targetVideo['url']);
+			$("#resultSection").removeClass('d-none');
+		}
+		catch(e){
+			$("#videoUrl").addClass('is-invalid');
+		}
+		$("#loadingSection").addClass('d-none');
+		$('#videoSubmitButton').attr("disabled", false);
+	});
+}
